b7ca692d2c699111481a0824cb5edb5e3a4462eb
ByZirTox <xxtedyxxrenard@gmail.com>
2023-01-11 17:34:20 +0100

    Save

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -11,6 +11,7 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Form\FormView;
 
 class RegistrationFormType extends AbstractType
 {
