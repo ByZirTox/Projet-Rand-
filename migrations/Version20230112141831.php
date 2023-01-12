98fafab3c7b0072fadad627e44a184ae81146fdc
ByZirTox <xxtedyxxrenard@gmail.com>
2023-01-12 15:34:07 +0100

    save

--- migrations/Version20230112141831.php
+++ migrations/Version20230112141831.php
@@ -31,6 +31,18 @@ final class Version20230112141831 extends AbstractMigration
         $this->addSql('ALTER TABLE user ADD vehicules_id INT DEFAULT NULL');
         $this->addSql('ALTER TABLE user ADD CONSTRAINT FK_8D93D6498D8BD7E2 FOREIGN KEY (vehicules_id) REFERENCES vehicules (id)');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D6498D8BD7E2 ON user (vehicules_id)');
+        $this->addSql('CREATE TABLE location (id INT AUTO_INCREMENT NOT NULL, vehicules_id INT DEFAULT NULL, user_id INT DEFAULT NULL, UNIQUE INDEX UNIQ_5E9E89CB8D8BD7E2 (vehicules_id), UNIQUE INDEX UNIQ_5E9E89CBA76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE location ADD CONSTRAINT FK_5E9E89CB8D8BD7E2 FOREIGN KEY (vehicules_id) REFERENCES vehicules (id)');
+        $this->addSql('ALTER TABLE location ADD CONSTRAINT FK_5E9E89CBA76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
+        $this->addSql('ALTER TABLE amendes DROP FOREIGN KEY FK_10B4203E8D8BD7E2');
+        $this->addSql('DROP INDEX IDX_10B4203E8D8BD7E2 ON amendes');
+        $this->addSql('ALTER TABLE amendes DROP vehicules_id');
+        $this->addSql('ALTER TABLE user ADD accident_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE user ADD CONSTRAINT FK_8D93D64916D8554C FOREIGN KEY (accident_id) REFERENCES accident (id)');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D64916D8554C ON user (accident_id)');
+        $this->addSql('ALTER TABLE vehicules ADD amendes_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE vehicules ADD CONSTRAINT FK_78218C2D113C5E8D FOREIGN KEY (amendes_id) REFERENCES amendes (id)');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_78218C2D113C5E8D ON vehicules (amendes_id)');
     }
 
     public function down(Schema $schema): void
@@ -47,5 +59,17 @@ final class Version20230112141831 extends AbstractMigration
         $this->addSql('DROP TABLE vehicules');
         $this->addSql('DROP INDEX UNIQ_8D93D6498D8BD7E2 ON user');
         $this->addSql('ALTER TABLE user DROP vehicules_id');
+        $this->addSql('ALTER TABLE location DROP FOREIGN KEY FK_5E9E89CB8D8BD7E2');
+        $this->addSql('ALTER TABLE location DROP FOREIGN KEY FK_5E9E89CBA76ED395');
+        $this->addSql('DROP TABLE location');
+        $this->addSql('ALTER TABLE vehicules DROP FOREIGN KEY FK_78218C2D113C5E8D');
+        $this->addSql('DROP INDEX UNIQ_78218C2D113C5E8D ON vehicules');
+        $this->addSql('ALTER TABLE vehicules DROP amendes_id');
+        $this->addSql('ALTER TABLE user DROP FOREIGN KEY FK_8D93D64916D8554C');
+        $this->addSql('DROP INDEX UNIQ_8D93D64916D8554C ON user');
+        $this->addSql('ALTER TABLE user DROP accident_id');
+        $this->addSql('ALTER TABLE amendes ADD vehicules_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE amendes ADD CONSTRAINT FK_10B4203E8D8BD7E2 FOREIGN KEY (vehicules_id) REFERENCES vehicules (id) ON UPDATE NO ACTION ON DELETE NO ACTION');
+        $this->addSql('CREATE INDEX IDX_10B4203E8D8BD7E2 ON amendes (vehicules_id)');
     }
 }
