593818be16f07a6e0c24cb057276033e7d97de88
NefytZ <thyphen45@live.fr>
2023-01-13 03:44:23 +0100

    vehicle_page_working

--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -8,19 +8,5 @@ export const app = startStimulusApp(require.context(
 ));
 
 // register any custom, 3rd party controllers here
-app.register('VehiculeController', VehiculeController);
-
-$(document).ready(function() {
-    $("#news-slider").owlCarousel({
-        items : 3,
-        itemsDesktop:[1199,3],
-        itemsDesktopSmall:[980,2],
-        itemsMobile : [600,1],
-        navigation:true,
-        navigationText:["",""],
-        pagination:true,
-        autoPlay:true
-    });
-});
-
+// app.register('some_controller_name', SomeImportedController);
 
